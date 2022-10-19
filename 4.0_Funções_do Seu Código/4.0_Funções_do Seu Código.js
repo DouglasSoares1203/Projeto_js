310dc6762baedee221e9bec0d868da98784067ec
DouglasSoares1203 <douglashardware13@gmail.com>
2022-10-18 23:56:20 -0300

    atualização...

--- 4.0_Funções_do Seu Código/4.0_Funções_do Seu Código.js
+++ 4.0_Funções_do Seu Código/4.0_Funções_do Seu Código.js
@@ -2,16 +2,23 @@ function calcularImc (peso,altura) {
     return peso / Math.pow(altura,2)
 }
 
-function ClassificacaoImc(imc) {
+function classificarImc(imc) {
     if (imc < 18.5) {
-        console.log('Abaixo do peso');
+        return 'Abaixo do peso';
     }else if(imc >= 18.5 && imc < 25) {
-        console.log('Peso normal');
+        return 'Peso normal';
     }else if(imc >= 25 && imc < 30){
-        console.log('Acima do peso');
+        return 'Acima do peso';
     }else if(imc >= 30 && imc < 40){
-        console.log('Obeso');
+        return 'Obeso';
     }else{
-        console.log('Obesidade grave');
+        return 'Obesidade grave';
     }
 }
+
+(function () {
+    const peso = 75;
+    const altura = 1.75;
+    const imc = calcularImc(peso,altura);
+    console.log(classificarImc(imc));
+})();
