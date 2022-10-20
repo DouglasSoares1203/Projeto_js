b6d4a6444de2085e48be6f14fc92a09f0f4f7baa
DouglasSoares1203 <douglashardware13@gmail.com>
2022-10-20 12:00:47 -0300

    atualizando os exercicios

---
+++ 6.0_Arrays_Estruturas_Repetição/6.1_Arrays_Estruturas_Repetição.js
@@ -0,0 +1,35 @@
+// 1)
+
+// const numero = 10;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * numero);
+    
+// }
+// -----------------------------------------------------
+
+//2)
+
+// const numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+// for (let i = 0; i < numeros.length; i++) {
+//     const numero = numeros[i];
+//     if (numero % 2 === 0) {
+//         console.log(numero)
+//     }
+// }
+// -----------------------------------------------------
+
+//3)
+
+// const nome = ['jose', 'vitor', 'vinicius', 'mario', 'vanessa']
+// let nomeComV = [];
+// for (let i = 0; i < nome.length; i++) {
+//     if (nome[i][0] === 'v') {
+//         nomeComV.push(nome[i])
+//     };
+
+
+    
+// }
+
+console.log(nomeComV);
